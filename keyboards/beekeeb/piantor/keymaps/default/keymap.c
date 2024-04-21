548ea363d354d0848be629f6ca589f6c8470084e
Frank Benoit <frank.benoit@etas.com>
2024-04-21 12:36:37 +0200

    modifiered keys

--- keyboards/beekeeb/piantor/keymaps/default/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/default/keymap.c
@@ -24,22 +24,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       * │   │ G │ X │ J │ K │ Z │       │ Q │ R │ M │ F │ P │   │
       * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
       *               ┌───┐                   ┌───┐
-      *               │Bsp├───┐           ┌───┤Spc│
-      *               └───┤Esc├───┐   ┌───┤Ent├───┘
-      *                   └───┤   │   │   ├───┘
+      *               │   ├───┐           ┌───┤   │
+      *               └───┤Bsp├───┐   ┌───┤Spc├───┘
+      *                   └───┤Esc│   │Ent├───┘
       *                       └───┘   └───┘
       */
     [0] = LAYOUT_split_3x6_3(
         KC_NO,   KC_B,              MT(MOD_RALT,KC_Y), KC_O,              KC_U,              KC_QUOT,                 KC_SCLN,    KC_L,              KC_D,              MT(MOD_RALT,KC_W), KC_V,              KC_NO,
         KC_NO,   MT(MOD_LGUI,KC_C), MT(MOD_LALT,KC_I), MT(MOD_LCTL,KC_E), MT(MOD_LSFT,KC_A), KC_COMM,                 KC_DOT,     MT(MOD_LSFT,KC_H), MT(MOD_LCTL,KC_T), MT(MOD_LALT,KC_S), MT(MOD_LGUI,KC_N), KC_NO,
         KC_NO,   KC_G,              KC_X,              KC_J,              KC_K,              KC_Z,                    KC_Q,       KC_R,              KC_M,              KC_F,              KC_P,              KC_NO,
-                                                       LT(1,KC_BSPC),     LT(3,KC_ESC),      LT(2,KC_RALT),           LT(1,KC_RALT), LT(3,KC_ENT), LT(2,KC_SPC)
+                                                       LT(2,KC_RALT),     LT(1,KC_BSPC),     LT(3,KC_ESC),            LT(3,KC_ENT), LT(2,KC_SPC),  LT(1,KC_RALT)
     ),
      /*
       * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
       * │   │ ~ │ % │ / │ \ │ ` │       │Tab│ ->│End│ $ │Ins│   │
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │   │ # │ @ │ PD│ PU│   │       │   │<- │Hom│ ^ │RAl│   │
+      * │   │ # │ @ │ PD│ PU│   │       │STb│<- │Hom│ ^ │RAl│   │
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
       * │   │   │Del│ ↓ │ ↑ │   │       │Cap│ ( │ { │ } │ ) │   │
       * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // nav
     [1] = LAYOUT_split_3x6_3(
         KC_NO,   KC_TILD,    KC_PERC,   KC_PSLS,     KC_BSLS,    KC_GRV,                          KC_TAB,      KC_RGHT,   KC_END,     KC_DLR,     KC_INS,     KC_NO, 
-		KC_NO,   KC_HASH,    KC_AT,     KC_PGDN,     KC_PGUP,    KC_NO,                           KC_NO,       KC_LEFT,   KC_HOME,    KC_CIRC,    KC_RALT,    KC_NO, 
+		KC_NO,   KC_HASH,    KC_AT,     KC_PGDN,     KC_PGUP,    KC_NO,                           S(KC_TAB),   KC_LEFT,   KC_HOME,    KC_CIRC,    KC_RALT,    KC_NO, 
 		KC_NO,   KC_NO,      KC_DEL,    KC_DOWN,     KC_UP,      KC_NO,                           KC_CAPS,     KC_LPRN,   KC_LCBR,    KC_RCBR,    KC_RPRN,    KC_NO,
 		                                KC_TRNS,     KC_TRNS,    KC_TRNS,                         KC_TRNS,     KC_TRNS,   KC_TRNS
     ),
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
      /*
       * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
-      * │   │   │   │Und│   │PSc│       │   │F9 │F10│   │   │   │
+      * │   │   │   │Und│Red│PSc│       │   │F9 │F10│   │   │   │
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
       * │   │   │   │   │   │   │       │   │F5 │F6 │F7 │F8 │   │
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
@@ -93,9 +93,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       */
 	// function
     [3] = LAYOUT_split_3x6_3(
-        KC_NO,   KC_NO,      KC_NO,     KC_UNDO,     KC_NO,      KC_PSCR,                         KC_NO,       KC_F9,     KC_F10,     KC_F11,     KC_F12,     KC_NO,
-		KC_NO,   KC_F16,     KC_F15,    KC_F14,      KC_F13,     KC_NO,                           KC_NO,       KC_F5,     KC_F6,      KC_F7,      KC_F8,      KC_NO,
-		KC_NO,   KC_NO,      KC_CUT,    KC_COPY,     KC_PSTE,    KC_NO,                           KC_NO,       KC_F1,     KC_F2,      KC_F3,      KC_F4,      KC_NO, 
+        KC_NO,   KC_F16,     KC_F15,    KC_F14,      KC_F13,     KC_PSCR,                         KC_NO,       KC_F9,     KC_F10,     KC_F11,     KC_F12,     KC_NO,
+		KC_NO,   KC_NO,      KC_NO,     C(KC_Z),     C(KC_Y),    KC_NO,                           KC_NO,       KC_F5,     KC_F6,      KC_F7,      KC_F8,      KC_NO,
+		KC_NO,   KC_NO,      C(KC_X),   C(KC_C),     C(KC_V),    KC_NO,                           KC_NO,       KC_F1,     KC_F2,      KC_F3,      KC_F4,      KC_NO, 
 		                                KC_TRNS,     KC_TRNS,    KC_TRNS,                         KC_TRNS,     KC_TRNS,   KC_TRNS
     )
 };
